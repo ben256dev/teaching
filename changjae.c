49e522f006923fdf585731b9e6a07b51b11d69b4
ben256dev <benjamin@ben256.com>
2024-07-23 20:55:34 +0900

    finshed step 4 and 5

--- changjae.c
+++ changjae.c
@@ -5,16 +5,19 @@
  * -[x] 1. declare another int and initialize(=) it with a value
  * -[x] 2. add the two ints and print their sum with printf. what format specifier should you use?
  * -[x] 3. declare a string
- * -[ ] 4. print the string using printf and the string format specifier
- * -[ ] 5. write a for loop to do these steps 0-4 10 times
+ * -[x] 4. print the string using printf and the string format specifier
+ * -[x] 5. write a for loop to do these steps 0-4 10 times
 */
 
 int main(void)
 {
-	int x = 20;
+	int i = 0;
+	for( i=0;i<10;i++ )
+	{int x = 20;
 	int c = 10;
 	const char* poop = "stinky";
 	printf("%d%s,hello world!\n", x + c, poop);
+	}
 
 	return 0;
 }
