91de68b7f672c4842bbeb71a61b45d8a7b3fb526
ben256dev <benjamin@ben256.com>
2024-07-23 20:42:17 +0900

    finshed step 2

--- changjae.c
+++ changjae.c
@@ -3,7 +3,7 @@
 /*
  * -[x] 0. declare an int and initialize(=) it with a value
  * -[x] 1. declare another int and initialize(=) it with a value
- * -[ ] 2. add the two ints and print their sum with printf. what format specifier should you use?
+ * -[x] 2. add the two ints and print their sum with printf. what format specifier should you use?
  * -[ ] 3. declare a string
  * -[ ] 4. print the string using printf and the string format specifier
  * -[ ] 5. write a for loop to do these steps 0-4 10 times
@@ -13,7 +13,7 @@ int main(void)
 {
 	int x = 20;
 	int c = 10;
-	printf("hello world!\n");
+	printf("%d,hello world!\n", x + c);
 
 	return 0;
 }
