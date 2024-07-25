53c733b021f6fc53fc2b31686772e12522a94f2c
ben256dev <benjamin@ben256.com>
2024-07-25 17:51:00 +0900

    finished step 6,7,8

--- changjae.c
+++ changjae.c
@@ -9,9 +9,9 @@
  * -[x] 4. initialize the string
  * -[x] 5. print the string
  *
- * -[ ] 6. write an if statement
- * -[ ] 7. write a for loop to do something 5 times
- * -[ ] 8. write a while loop to do something 5 times
+ * -[x] 6. write an if statement
+ * -[x] 7. write a for loop to do something 5 times
+ * -[x] 8. write a while loop to do something 5 times
  *
  * -[ ] 9. write a for loop inside a for loop to do something 25 times
 */
@@ -19,9 +19,20 @@
 int main(void)
 {
 	//int is 4 bytes and 32 bits
+	int i = 3;
+	if (i< 3)
+		printf("perfect\n");
+	for (i=0; i<5; i++)
+	{
 	int smoothie = 100001;
 	const char* lemon = "ice";
 	printf("%d\n%s\n", smoothie, lemon);
+	}
+	while (i<10)
+	{ 
+		printf("this is a while loop\n");
+		i++;
+	}
 
 	return 0;
 }
