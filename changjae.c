d874a6159f71ca73cd24f1c6f131cdca63df3d1e
ben256dev <benjamin@ben256.com>
2024-07-25 18:02:10 +0900

    finished review

--- changjae.c
+++ changjae.c
@@ -13,26 +13,33 @@
  * -[x] 7. write a for loop to do something 5 times
  * -[x] 8. write a while loop to do something 5 times
  *
- * -[ ] 9. write a for loop inside a for loop to do something 25 times
+ * -[x] 9. write a for loop inside a for loop to do something 25 times
 */
 
 int main(void)
 {
-	//int is 4 bytes and 32 bits
-	int i = 3;
-	if (i< 3)
+	int a = 3;
+	if (a< 3)
 		printf("perfect\n");
-	for (i=0; i<5; i++)
+	for (int i=0; i<5; i++)
 	{
+		for(int j=0; j<5; j++)
+		{
+			printf("i : %d, j : %d\n", i, j);
+		}
+	}
+	
 	int smoothie = 100001;
 	const char* lemon = "ice";
 	printf("%d\n%s\n", smoothie, lemon);
-	}
-	while (i<10)
+
+	int w = 0;
+	while (w < 5)
 	{ 
 		printf("this is a while loop\n");
-		i++;
+		w++;
 	}
 
 	return 0;
 }
+
