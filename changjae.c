70fe357afc73b822bff735c5a5d897abb5b22fe9
ben256dev <benjamin@ben256.com>
2024-07-25 16:24:19 +0900

    start of Jul 25 review

--- changjae.c
+++ changjae.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /*
- * -[x] 0. declare an int and initialize(=) it with a value
- * -[x] 1. declare another int and initialize(=) it with a value
- * -[x] 2. add the two ints and print their sum with printf. what format specifier should you use?
- * -[x] 3. declare a string
- * -[x] 4. print the string using printf and the string format specifier
- * -[x] 5. write a for loop to do these steps 0-4 10 times
+ * -[ ] 0. declare an int
+ * -[ ] 1. initialize the int
+ * -[ ] 2. print the int
+ *
+ * -[ ] 3. declare a string
+ * -[ ] 4. initialize the string
+ * -[ ] 5. print the string
+ *
+ * -[ ] 6. write an if statement
+ * -[ ] 7. write a for loop to do something 5 times
+ * -[ ] 8. write a while loop to do something 5 times
+ *
+ * -[ ] 9. write a for loop inside a for loop to do something 25 times
 */
 
 int main(void)
 {
-	// 'for int i equals zero while i is less than ten i plus plus'
-	for (int i = 0; i < 10; i++)
-	{
-		int x = 20;
-		int c = 10;
-		const char* poop = "stinky";
-		printf("%d%s,hello world!\n", x + c, poop);
-	}
+	printf("hello\n");
 
 	return 0;
 }
