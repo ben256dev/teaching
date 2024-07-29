cd07779a2b0c1f225d87b1c6d240d9528a343a3c
ben256dev <benjamin@ben256.com>
2024-07-29 16:52:20 +0900

    code for Jul 29 review

--- changjae.c
+++ changjae.c
@@ -1,28 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_str(char* str)
-{
-	for (int i=0; str[i] != '\0'; i++)
-		printf("%c", str[i]);
-}
-
-int length_str(char* str)
-{
-	int i;
-	for (i = 0; str[i] != '\0'; i++) {};
-	return i;
-}
-
 int main(void)
 {
-	print_str("Hello\n");
-	print_str("World\n");
-	print_str("ChatGPT\n");
-	print_str("OpenAI\n");
-	print_str("CStrings\n");
-
-	printf("length_str : %d, strlen : %lu\n", length_str("Hello\n"), strlen("Hello\n"));
+	printf("hello!\n");
 
 	return 0;
 }
