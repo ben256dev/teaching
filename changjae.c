f25b1b104e03e3e042ced977032ce7c202b870c6
ben256dev <benjamin@ben256.com>
2024-07-29 19:59:26 +0900

    JUL29th lesson

--- changjae.c
+++ changjae.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	int a, b, c, d, e, f;
-	a = 7;
-	b = 3;
-	c = a / b;
-	d = a - b;
-	f = c * d;
+	srand(time(0));
+	
+	int random_number = rand() % 10;
+
+	printf("Guess the random number:\n");
+
+	int input = -1;
+	while (input != random_number)
+	{
+		if (input != -1)
+			printf("wrong number dumbass!\n");
+
+		scanf("%d", &input);
+	}
+	printf("\nyou win!\n");
 
 	return 0;
 }
