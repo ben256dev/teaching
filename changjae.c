ac59d392146a28efd1fe73ec580a6db046d46980
Changjae Lee <mica-teddy.04@icloud.com>
2024-07-30 14:38:21 +0900

    finished step 4

--- changjae.c
+++ changjae.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(void)
 {
+
 	printf("Hello World!\n");
 	int a;
 	const char* b;
+	
+	int a = rand() % 10
 
 	return 0;
 }
