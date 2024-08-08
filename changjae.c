ba366f1df280ef4b053aedb38b51f5f1124fa826
Changjae Lee <mica-teddy.04@icloud.com>
2024-08-08 16:50:56 +0900

    finished step2

--- changjae.c
+++ changjae.c
@@ -3,12 +3,20 @@
 
 int main(void)
 {
-	int numbers[5] = {1, 2, 3, 4, 5};
+	/*int numbers[5] = {1, 2, 3, 4, 5};
 
 	for (int i = 0; i < 5; i++)
 	{
 	printf("%d\n", numbers[i]);
-	}
+	}*/
+
+	int numbers[3][4] =
+	{
+		{1,2,3,4},
+		{5,6,7,8},
+		{9,10,11,12}
+	};
+
 
 	return 0;
 }
