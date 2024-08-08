bc869f96367f1413749a7bb8d7f5b7025122f983
Changjae Lee <mica-teddy.04@icloud.com>
2024-08-08 17:06:55 +0900

    finsished review

--- changjae.c
+++ changjae.c
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-	/*int numbers[5] = {1, 2, 3, 4, 5};
+	int num[5] = {1, 2, 3, 4, 5};
 
 	for (int i = 0; i < 5; i++)
 	{
-	printf("%d\n", numbers[i]);
-	}*/
+	printf("%d\n", num[i]);
+	}
 
 	int numbers[3][4] =
 	{
@@ -21,7 +21,7 @@ int main(void)
 	{
 		for (int j = 0; j < 4; j++)
 		{
-		printf("%d", numbers[i][j]);
+		printf("%d ", numbers[i][j]);
 		}
 		printf("\n");
 	}
